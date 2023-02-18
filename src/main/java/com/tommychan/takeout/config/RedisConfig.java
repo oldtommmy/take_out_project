15db43075aa9ea631ae3a6ae3c536a0984cc5222
Chan <88810398+oldtommmy@users.noreply.github.com>
2023-02-18 21:25:27 +0800

    完成Redis环境配置 第一阶段结束：完成后台和移动端基本功能 后台管理缺失功能： 1. 菜品管理停售、删除、批量起售、停售 2. 套餐管理停售、删除、批量起售、停售 3. 订单明细

---
+++ src/main/java/com/tommychan/takeout/config/RedisConfig.java
@@ -0,0 +1,26 @@
+package com.tommychan.takeout.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+
+        //修改 key 的序列化器
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        return redisTemplate;
+    }
+
+}
